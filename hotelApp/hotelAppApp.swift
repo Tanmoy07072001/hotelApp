fc4e9c2e3cfab7fc871f3aebc180d8c703f518f2
tanmoy <roytanmoy07072001@gmail.com>
2024-02-19 14:49:53 +0530

    Initial Commit

---
+++ hotelApp/hotelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  hotelAppApp.swift
+//  hotelApp
+//
+//  Created by TANMOY ROY on 19/02/24.
+//
+
+import SwiftUI
+
+@main
+struct hotelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
